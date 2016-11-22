d9cdb6bfd5d899dcd7746209a4f755d9ef2aaccc
Alastair <alsgregory@aol.com>
2016-11-22 14:50:16 +0000

    install instr for pulp for emd in readme. spaces in emd

--- firedrake_da/emd.py
+++ firedrake_da/emd.py
@@ -1,4 +1,4 @@
-""" Earth Mover's Distance / Ensemble Transform functions (Courtesy of O. Pele and M. Werman (2009)) """
+""" Earth Mover's Distance / Ensemble Transform functions (Algorithm courtesy of O. Pele and M. Werman (2009)) """
 
 from __future__ import division
 
@@ -12,31 +12,43 @@ import re
 
 
 def onedtransform(wa, wb, pa):
+
     w1 = np.copy(wa)
     w2 = np.copy(wb)
+
     N = len(w1)
+
     ensembletransform = np.zeros(N)
+
     i = N
     j = N
+
     while i * j >= 1:
+
         if w1[i - 1] < w2[j - 1]:
             ensembletransform[j - 1] += (w1[i - 1] * (1.0 / wb[j - 1])) * pa[i - 1]
             w2[j - 1] = w2[j - 1] - w1[i - 1]
             i = i - 1
+
         else:
             ensembletransform[j - 1] += (w2[j - 1] * (1.0 / wb[j - 1])) * pa[i - 1]
             w1[i - 1] = w1[i - 1] - w2[j - 1]
             j = j - 1
+
     return ensembletransform
 
 
 def transform(feature1, feature2, w1, w2, Cost):
+
     H = feature1.shape[1]
     I = feature2.shape[1]
     D = feature1.shape[0]
+
     wa = np.copy(w1)
     wb = np.copy(w2)
+
     distances = Cost
+
     # Set variables for EMD calculations
     variablesList = []
     for i in range(H):
@@ -45,6 +57,7 @@ def transform(feature1, feature2, w1, w2, Cost):
             tempList.append(LpVariable("x" + str(i) + " " + str(j), lowBound=0))
         variablesList.append(tempList)
     problem = LpProblem("EMD", LpMinimize)
+
     # objective function
     constraint = []
     objectiveFunction = []
@@ -55,6 +68,7 @@ def transform(feature1, feature2, w1, w2, Cost):
     problem += lpSum(objectiveFunction)
     tempMin = min(sum(wa), sum(wb))
     problem += lpSum(constraint) == tempMin
+
     # constraints
     for i in range(H):
         constraint1 = [variablesList[i][j] for j in range(I)]
@@ -62,18 +76,21 @@ def transform(feature1, feature2, w1, w2, Cost):
     for j in range(I):
         constraint2 = [variablesList[i][j] for i in range(H)]
         problem += lpSum(constraint2) == wb[j]
+
     # solve
     problem.writeLP("EMD.lp")
     problem.solve(GLPK_CMD(msg=False))
     OptimalMatrix = np.zeros((H, I))
     new_feature1 = np.zeros((D, H))
     for variable in problem.variables():
+
         if str(variable) is not '__dummy':
             [i, j] = re.findall('\d+', str(variable))
             ii = int(i)
             jj = int(j)
             OptimalMatrix[ii, jj] = variable.varValue
             new_feature1[:, jj] += feature1[:, ii] * (1.0 / wb[jj]) * OptimalMatrix[ii, jj]
+
     return new_feature1
 
 
