885dcddd683d876c1919cf894da450aa31804b49
Alastair Gregory <alsgregory@aol.com>
2017-01-06 13:33:07 +0000

    added mrh tests

---
+++ tests/verification/test_mrh.py
@@ -0,0 +1,68 @@
+from __future__ import division
+
+from __future__ import absolute_import
+
+from firedrake import *
+
+from firedrake_da import *
+from firedrake_da.ml import *
+
+import numpy as np
+
+
+def test_mrh_n_equals_1():
+
+    mesh = UnitIntervalMesh(2)
+    fs = FunctionSpace(mesh, 'DG', 0)
+    f = Function(fs).assign(1)
+
+    obs = np.array([0.0, 0.0])
+    coords = np.array([np.array([0.25]), np.array([0.75])])
+
+    # set-up ensemble
+    ensemble = [f]
+
+    # set-up rank histogram class
+    R = rank_histogram(1)
+    for i in range(20):
+        R.compute_rank(ensemble, coords, obs)
+
+    assert np.all(R.ranks[0] >= 0.0) and np.all(R.ranks[0] <= 0.5)
+
+
+def test_mrh_2_obs():
+
+    mesh = UnitIntervalMesh(2)
+    fs = FunctionSpace(mesh, 'DG', 0)
+    f = Function(fs).assign(1)
+
+    obs = np.array([0.0, 2.0])
+    coords = np.array([np.array([0.25]), np.array([0.75])])
+
+    # set-up ensemble
+    ensemble = [f]
+
+    # set-up rank histogram class
+    R = rank_histogram(1)
+    for i in range(20):
+        R.compute_rank(ensemble, coords, obs)
+
+    assert np.all(R.ranks[0] >= 0.0) and np.all(R.ranks[0] <= 1.0)
+
+
+def test_choose_uniform_rank():
+
+    n = 500000
+    ranks = np.zeros(n)
+
+    for i in range(n):
+        R = rank_histogram(1)
+        ranks[i] = R._rank_histogram__choose_uniform_rank(0, 1)
+
+    assert np.abs(np.mean(ranks) - 0.5) < 2e-3
+
+
+if __name__ == "__main__":
+    import os
+    import pytest
+    pytest.main(os.path.abspath(__file__))
